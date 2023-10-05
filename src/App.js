6a7e78c529b1ccc0810a9ebcf9678edfbfafa915
Geetanjali-18 <104240730+Geetanjali-18@users.noreply.github.com>
2023-10-05 08:51:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar  from './components/Navbar/Navbar';
+import Banner from './components/Banner/Banner';
+import "./App.css"
+
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Banner/>
+    </>
+  );
+}
+
+export default App;
